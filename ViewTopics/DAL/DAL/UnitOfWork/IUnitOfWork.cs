19e0d151f05c0018f022133c55e3227acfc325ec
AlexandrChornyi <65231209+AlexandrChornyi@users.noreply.github.com>
2020-06-05 19:41:21 +0300

    Added IUnitOfWork

---
+++ ViewTopics/DAL/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository users { get; }
+        ICategoryRepository categories { get; }
+        ICategoryTopicRepository categoryTopics { get; }
+        ITopicRepository topics { get; }
+        void Save();
+    }
+}
