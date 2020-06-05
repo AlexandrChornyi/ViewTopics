458318e85d3fd197c00946cb4a198b15d3b6689d
AlexandrChornyi <65231209+AlexandrChornyi@users.noreply.github.com>
2020-06-05 18:54:50 +0300

    Added categoryContext

---
+++ ViewTopics/DAL/DAL/EF/categoryContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+
+namespace DAL.EF
+{
+    public class categoryContext
+        : DbContext
+    {
+        public DbSet<topic> Topics{ get; set; }
+        public DbSet<users> Users { get; set; }
+        public DbSet<category> Categories { get; set; }
+        public DbSet<category_topic> CategoryTopics { get; set; }
+        public categoryContext(DbContextOptions options)
+            : base(options) { }
+    }
+}
