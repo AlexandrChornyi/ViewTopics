4f3c459376ff33d5a28b1558114786d93dfba089
AlexandrChornyi <65231209+AlexandrChornyi@users.noreply.github.com>
2020-06-06 13:36:13 +0300

    Added identities and SecurityContext

---
+++ ViewTopics/CLL/Security/Identity/Moder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLL.Security.Identity
+{
+    public class Moder 
+        : User
+    {
+        public Moder(int userId, string name, string categoryId)
+            : base(userId, name, categoryId, nameof(Moder))
+        {
+        }
+    }
+}
---
+++ ViewTopics/CLL/Security/Identity/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CLL.Security.Identity
+{
+    public abstract class User
+    {
+        public User(int userId, string name, string categoryId, string userType)
+        {
+            User_Id = userId;
+            User_Name = name;
+            Category_ID = categoryId;
+            User_Type = userType;
+        }
+        public int User_Id { get; }
+        public string Category_ID { get; }
+        public string User_Name { get; }
+        protected string User_Type { get; }
+    }
+}
